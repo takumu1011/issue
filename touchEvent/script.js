a0de574f864cb58a760e4f728fcdcc455bfdbfa9
tkm <sumikawa.web@gmail.com>
2020-10-20 23:21:27 +0900

    touchEvent

---
+++ touchEvent/script.js
@@ -0,0 +1,11 @@
+// window.addEventListener('touchstart', function () {
+//   alert('touchstart');
+// });
+
+// window.addEventListener('touchend', function () {
+//   alert('touchend');
+// });
+
+window.addEventListener('touchmove', function () {
+  alert('touchmove');
+});
